0d4a75a4e1b965e7e59b94904d9b674bd6b88cfb
WellysonP <wellysonpontes@gmail.com>
2023-01-19 23:42:10 -0300

    encontando o erro

--- email-worker-compose/app/sender.py
+++ email-worker-compose/app/sender.py
@@ -7,31 +7,31 @@ from bottle import Bottle, request
 class Sender(Bottle):
     def __init__(self):
         super().__init__()
-        self.route('/', method="POST", callback=self.send)
+        self.route('/', method='POST', callback=self.send)
         self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
-        DSN = 'dbname=email_sender user=postgres password=example host=db'
+
+        DSN = 'dbname=email_sender user=postgres password=exemple host=db'
         self.conn = psycopg2.connect(DSN)
 
-def register_message(self, assunto, mensagem):
-    SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
-    cur = self.conn.cursor()
-    cur.execute(SQL, (assunto, mensagem))
-    self.conn.commit()
-    cur.close()
+    def register_message(self, assunto, mensagem):
+        SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
 
-    msg = {'assunto': assunto, 'mensagem': mensagem}
-    self.fila.rpush('sender', json.dumps(msg))
+        cur = self.conn.cursor()
+        cur.execute(SQL, (assunto, mensagem))
+        self.conn.commit()
+        cur.close()
 
-    print('Mensagem registrada !')
+        msg = {'assunto': assunto, 'mensagem': mensagem}
+        self.fila.rpush('sender', json.dumps(msg))
+        print('Mensagem registrada !')
 
-def send(self):
-    assunto = request.forms.get('assunto')
-    mensagem = request.forms.get('mensagem')
+    def send(self):
+        assunto = request.forms.get('assunto')
+        mensagem = request.forms.get('mensagem')
+        self.register_message(assunto, mensagem)
+        return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
+        assunto, mensagem)
 
-    self.register_message(assunto, mensagem)
-    return 'Mensagem enfileirada ! Assunto: {}, Mensagem: {}'.format(
-        assunto, mensagem
-    )
 
 if __name__ == '__main__':
     sender = Sender()
